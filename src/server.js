ff86af462c4db13249b62abdefdefdce6535e072
Carlos Eduardo <carlos.oliveira@fiap.com.br>
2024-03-18 06:46:42 -0300

    feat: Create Middlewares

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@
 // Padrao node nao suporta ESMoudes alterar no package.json
 
 import http from "node:http";
+import { json } from "./middlewares/json.js";
 
 // - HTTP
 //  - Metodos HTTP
@@ -41,23 +42,11 @@ const users = [];
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
 
-  const buffers = [];
-
-  for await (const chunk of request) {
-    buffers.push(chunk);
-  }
-
-  try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    request.body = null;
-  }
+  await json(request, response);
 
   if (method === "GET" && url === "/users") {
     // Early return
-    return response
-      .setHeader("Content-Type", "application/json")
-      .end(JSON.stringify(users));
+    return response.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
